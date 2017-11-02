f0753761b5fea0d92c2140b896a5a35b6fe003ca
Keith <Keith@Keith-PC>
2017-11-01 21:40:49 -0500

    Move reporting code

--- src/org/firebears/util/RoboReport.java
+++ src/org/firebears/util/RoboReport.java
@@ -3,6 +3,7 @@ package org.firebears.util;
 import java.io.PrintStream;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TreeSet;
@@ -13,6 +14,7 @@ import edu.wpi.first.wpilibj.PowerDistributionPanel;
 public class RoboReport {
 	
 	private final static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd");
+	private final static DecimalFormat numberFormat = new DecimalFormat("###,###,###,###,##0");
 
 	public static void print(PrintStream out) {
 		out.printf("# RoboReport, as of %s%n", dateFormat.format(new Date()));
@@ -30,7 +32,7 @@ public class RoboReport {
 		PowerDistributionPanel pdp = new PowerDistributionPanel();
 		out.printf("* voltage = %5.2f%n", pdp.getVoltage());
 		out.printf("* current = %5.2f%n", pdp.getTotalCurrent());
-		out.printf("* PDP temp = %5.2f%n", pdp.getTemperature());
+		out.printf("* PDP temp = %5.2f C%n", pdp.getTemperature());
 		out.println();
 		
 //		out.println("## CAN");
@@ -45,9 +47,9 @@ public class RoboReport {
 		
 		out.println("## System Properties");
 		out.println();
-		for (String varName : (new TreeSet<String>(System.getenv().keySet()))) {
-			if (varName.equals("line.separator")) continue;
-			out.printf("* %s = %s%n", varName, System.getenv(varName));
+		for (String propName : (new TreeSet<String>(System.getProperties().stringPropertyNames()))) {
+			if (propName.equals("line.separator"))  continue;
+			out.printf("* %s = %s%n", propName, System.getProperty(propName));
 		}
 		out.println();
 		
@@ -60,10 +62,10 @@ public class RoboReport {
 		
 		out.println("## Java Runtime");
 		out.println();
-		out.printf("* availableProcessors : %d%n", Runtime.getRuntime().availableProcessors());
-		out.printf("* totalMemory : %d%n", Runtime.getRuntime().totalMemory());
-		out.printf("* maxMemory : %d%n", Runtime.getRuntime().maxMemory());
-		out.printf("* freeMemory : %d%n", Runtime.getRuntime().freeMemory());
+		out.printf("* availableProcessors = %d%n", Runtime.getRuntime().availableProcessors());
+		out.printf("* totalMemory = %s%n", numberFormat.format(Runtime.getRuntime().totalMemory()));
+		out.printf("* maxMemory = %s%n", numberFormat.format(Runtime.getRuntime().maxMemory()));
+		out.printf("* freeMemory = %s%n", numberFormat.format(Runtime.getRuntime().freeMemory()));
 		out.println();
 		
 		out.println("## Java classpath");
