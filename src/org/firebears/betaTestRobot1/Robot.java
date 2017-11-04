f9a4717cbf65975d224af8601a0fe13f504f9ae1
Keith <Keith@Keith-PC>
2017-11-04 16:29:02 -0500

    Update RobotReport

--- src/org/firebears/betaTestRobot1/Robot.java
+++ src/org/firebears/betaTestRobot1/Robot.java
@@ -1,11 +1,13 @@
 package org.firebears.betaTestRobot1;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
 
+import java.io.File;
 import java.io.PrintStream;
 
 import org.firebears.util.CANTalon;
-import org.firebears.util.RoboReport;
+import org.firebears.util.RobotReport;
 
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -14,10 +16,12 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 /**
- * Simple program to verify that we can load code into the roboRIO and 
- * that we can communicate to other parts of the robot.
+ * Simple program to verify that we can load code into the roboRIO and that we
+ * can communicate to other parts of the robot.
  */
 public class Robot extends IterativeRobot {
+
+	public static final int MOTOR_CAN_ID = 2;
 
 	Joystick joystick = null;
 	CANTalon motor = null;
@@ -27,16 +31,22 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void robotInit() {
-		
+
+		RobotReport report = new RobotReport("betaTestRobot1");
+		report.setDescription("IterativeRobot for testing the 2018 beta software.");
+
 		// All robot components should be created in the robotInit method
 		joystick = new Joystick(0) {
 			public int getAxisCount() {
 				// temporary hack to get around bug in Joystick class.
-				int superCount = super.getAxisCount();
-				return 5;
+				return max(5, super.getAxisCount());
 			}
 		};
-		motor = new CANTalon(2);
+		report.addJoystick(0, "Main Joystick", joystick);
+
+		motor = new CANTalon(MOTOR_CAN_ID);
+		report.addCAN(MOTOR_CAN_ID, "Motor", motor);
+
 		driverStation = DriverStation.getInstance();
 		pdp = new PowerDistributionPanel();
 
@@ -44,8 +54,9 @@ public class Robot extends IterativeRobot {
 		motor.clearStickyFaults();
 		pdp.clearStickyFaults();
 		(new Compressor()).clearAllPCMStickyFaults();
-		
-		RoboReport.print(System.out);
+
+		report.write(System.out);
+		report.write(new File("/home/lvuser/robotReport.md"));
 	}
 
 	@Override
